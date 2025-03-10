8813287760b136d31511e2f078084ba4c99e7e6c
AgwuChukwuma <Agwuchukwuma1@gmail.com>
2025-03-10 16:12:54 +0100

    Update SearchSelection.jsx

--- weather-app/src/componenets/SearchSelection.jsx
+++ weather-app/src/componenets/SearchSelection.jsx
@@ -1,29 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const SearchSelection = ({ getWeatherDetails }) =>{
+const SearchSelection = ({ getWeatherDetails }) => {
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
+  const handleCitySearch = (e) => {
+    e.preventDefault();
+    const searchInput = e.target.querySelector(".search-input");
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}`;
+    getWeatherDetails(API_URL);
+  };
 
-    const API_KEY = import.meta.env.VITE_API_KEY;
-    console.log(API_KEY);
-    const handleCitySearch = (e) =>{console.log(API_KEY);
-        e.preventDefault();
-        const searchInput = e.target.querySelector(".search-input");
-        const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}`;
+  const handleLocationSearch = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}`;
         getWeatherDetails(API_URL);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
     }
-    return(
+  };
 
-<div className="search-section">
+  return (
+    <div className="search-section">
       <form action="#" className="search-form" onSubmit={handleCitySearch}>
-      <span className="material-symbols-rounded">search</span>
-        <input type="search" placeholder="Enter a city name"
-         className="search-input" required />
+        <span className="material-symbols-rounded">search</span>
+        <input
+          type="search"
+          placeholder="Enter a city name"
+          className="search-input"
+          required
+        />
       </form>
-      <button className="location-button">
-      <span className="material-symbols-rounded">my_location</span>
+      <button className="location-button" onClick={handleLocationSearch}>
+        <span className="material-symbols-rounded">my_location</span>
       </button>
-      </div>
-    )
-}
+    </div>
+  );
+};
 
-export default SearchSelection
+SearchSelection.propTypes = {
+  getWeatherDetails: PropTypes.string.isRequired,
+};
+export default SearchSelection;
